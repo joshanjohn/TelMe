6bb928982e4db7ecdc707988be71bda0e1519819
Joshan john <joshanjohn2003@gmail.com>
2024-09-06 08:45:21 +0100

    Create a custom splash screen

--- telme/lib/views/splash_screen.dart
+++ telme/lib/views/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -21,9 +22,114 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Splash Screen", style: TextStyle(fontSize: 32),),
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Positioned(
+              left: -100,
+              top: -20,
+              child: Container(
+                height: 600,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 177, 127, 242),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: -100,
+              right: -100,
+              child: Container(
+                height: 600,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 152, 100, 198),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: -20,
+              right: -100,
+              child: Container(
+                height: 200,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 210, 100, 207),
+                  borderRadius: BorderRadius.circular(150),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: -100,
+              bottom: -50,
+              child: Container(
+                height: 200,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 98, 91, 196),
+                  borderRadius: BorderRadius.circular(150),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "TEL\n ME",
+                      style: TextStyle(
+                          fontSize: 100,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 59, 19, 82)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: CircularProgressIndicator(
+                        color: Color.fromARGB(229, 222, 200, 102),
+                        strokeWidth: 10,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
